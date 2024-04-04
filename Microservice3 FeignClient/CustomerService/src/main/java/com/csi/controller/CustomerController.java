6ad3ee57500b05e08038f12bf3e5629b390e7e51
shubhamchande12147879 <shubhamchande839@gmail.com>
2024-04-04 16:24:53 +0530

    [ST NO 122] Added logging functinality

--- Microservice3 FeignClient/CustomerService/src/main/java/com/csi/controller/CustomerController.java
+++ Microservice3 FeignClient/CustomerService/src/main/java/com/csi/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.csi.controller;
 import com.csi.model.Customer;
 import com.csi.service.CustomerServiceImpl;
 import com.csi.vo.RestTemplateVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +13,20 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/customers")
+@Slf4j
 public class CustomerController {
     @Autowired
     private CustomerServiceImpl customerServiceImpl;
     @PostMapping("/savecustomer")
     public ResponseEntity<Customer> saveCustomerDate(@Valid @RequestBody Customer customer){
+        log.info("SAVE TO CUSTOME DATA FOR "+customer.getCustName());
         return new ResponseEntity<>(customerServiceImpl.saveCustomer(customer), HttpStatus.CREATED);
     }
 
     @GetMapping("/findbycustid/{custId}")
     public ResponseEntity<RestTemplateVo> findBycustId(@PathVariable int custId){
+        log.info("Find THE DATA ON THE BASIS OF ID ");
         return ResponseEntity.ok(customerServiceImpl.findBycustId(custId));
     }
+
 }
